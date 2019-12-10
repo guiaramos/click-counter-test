ce7ba948dcae5ecd61949a3758ceebc7ae6dda86
Guilherme Ramos <gui_aramos@outlook.comm>
2019-12-10 21:17:43 +0900

    Fix: small bugs

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
-import { wrap } from 'module';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
